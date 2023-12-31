5347599bcc541d90f8f8562b5284466c2cc7f812
xrzse <atmanav04@gmail.com>
2023-12-30 23:57:27 -0500

    coding.

--- javascriptintro/index.js
+++ javascriptintro/index.js
@@ -1,2 +1,12 @@
-// for javascript you add ; at the end
-console.log('Hello Kundi');
+let name = 'Mosh'; 
+console.log(name);
+
+// let variables can change overtime
+let interestRate = 0.3;
+interestRate = 1
+console.log(interestRate)
+
+// However const variables cannot. We get the error assignment to constant variable.
+const interestRate1 = 0.3;
+interestRate1 = 1
+console.log(interestRate1)
